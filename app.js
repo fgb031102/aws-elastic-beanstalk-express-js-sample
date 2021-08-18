b20b5a89a772660696954fa091073d6fe8effd88
linkjapan <fon@linkjapan.co.jp>
2021-08-18 15:28:51 +0900

    token

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Hello!'));
+app.get('/', (req, res) => res.send('Hello Hello! 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'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
